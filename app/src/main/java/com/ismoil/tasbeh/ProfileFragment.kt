8ca40f3a1003b068541954f0f2e67aed773adaed
이스마일 <ismoil@MacBook-Pro-iseumail.local>
2025-05-29 15:31:48 +0900

    navigation in profile next mainfragment

--- app/src/main/java/com/ismoil/tasbeh/ProfileFragment.kt
+++ app/src/main/java/com/ismoil/tasbeh/ProfileFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ismoil.tasbeh.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
 
 
+    private lateinit var  binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        binding.btnClose.setOnClickListener {
+            findNavController().navigate(R.id.mainFragment);
+        }
     }
 
 
